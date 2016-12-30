0c66c6b5b617f4062b908ee20d77593deb76b58c
Fábio Antunes <fabioantuness@gmail.com>
2016-12-30 16:28:15 +0000

    Added support for yarn and custom functions

--- __nvm_can_run.fish
+++ __nvm_can_run.fish
@@ -1,6 +1,6 @@
 function __nvm_can_run
-  if type -P $argv[1] > /dev/null 2>&1
-      return
+  if type -P $argv[1] > /dev/null 2>&1 or type -P node > /dev/null 2>&1
+    return
   else
     return 1
   end
---
+++ yarn.fish
@@ -0,0 +1,3 @@
+function yarn -d "yarn package manager" -w yarn
+  __nvm_run "yarn" $argv
+end
