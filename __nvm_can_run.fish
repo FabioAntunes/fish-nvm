50db93fabaa1168c301d0e1fad7dc6b1d98ecfa7
Will Dawson <william.dawson@mi9.com.au>
2017-01-17 15:16:48 +1100

    Fix missing semi-colon breaking __nvm_can_run.

--- __nvm_can_run.fish
+++ __nvm_can_run.fish
@@ -1,5 +1,5 @@
 function __nvm_can_run
-  if type -P $argv[1] > /dev/null 2>&1 or type -P node > /dev/null 2>&1
+  if type -P $argv[1] > /dev/null 2>&1; or type -P node > /dev/null 2>&1
     return
   else
     return 1
