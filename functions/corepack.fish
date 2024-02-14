c19e28a784f1616e269b6177cd9eee76eb48c679
Szymon Polny <szymonpolny@outlook.com>
2024-02-14 11:03:07 +0100

    added corepack and pnpm support

---
+++ functions/corepack.fish
@@ -0,0 +1,3 @@
+function corepack -d "tool to manage package managers" -w corepack
+    __nvm_run "corepack" $argv
+end
---
+++ functions/pnpm.fish
@@ -0,0 +1,3 @@
+function pnpm -d "disk space efficient package manager" -w pnpm
+    __nvm_run "pnpm" $argv
+end
